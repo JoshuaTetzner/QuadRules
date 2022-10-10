605131f8697bae407cb98e2801f4bc7b06e235c4
JoshuaTet <joshua.tetzner@gmx.de>
2022-10-10 17:01:34 +0200

    added Sommariva rules

--- examples/balancednodeelimination.jl
+++ examples/balancednodeelimination.jl
@@ -2,13 +2,33 @@ using JLD2
 using FastGaussQuadrature
 
 # asymmetric cubature for a complete polynomial system of degree n.
-n = 9
+n = 6
 order = 2*n-1 
 xa, wa = gausslegendre(n) 
 nodes, weights = tensorrule(xa, wa, xa, wa, 2)
-nodes, weights = balancednonsymmetricquad(nodes, weights, order-1)
-println(nodes)
-println(weights)
+@time nodes, weights = balancednonsymmetricquad(nodes, weights, order-1)
+#println(nodes)
+#println(weights)
 ## 
 
+function asymcub(a::Int, b::Int)
+    order = a
+    points = Int(ceil((order+1)/2))
+    xa, wa = gausslegendre(points) 
+    nodes, weights = tensorrule(xa, wa, xa, wa, 2)
+    nodes, weights = balancednonsymmetricquad(nodes, weights, order)
+    dict = Dict{String, Any}(string(order) => Dict("weights" => weights, "nodes" => nodes))
+    save("nonsymmetric" * string(order) * ".jld2", dict)
+
+    for order = (a+1):b
+        points = Int(ceil((order+1)/2))
+        xa, wa = gausslegendre(points) 
+        nodes, weights = tensorrule(xa, wa, xa, wa, 2)
+        nodes, weights = balancednonsymmetricquad(nodes, weights, order)
+        dict = Dict{String, Any}(string(order) => Dict("weights" => weights, "nodes" => nodes))
+        save("nonsymmetric" * string(order) * ".jld2", dict)
+    end
+end
+
+asymcub(15,23)
 
